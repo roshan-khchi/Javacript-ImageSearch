244e04a344fe2541d5210b76b538d7c4d3b9f63b
roshan-khichi <rokhichi550@gamil.com>
2024-01-21 21:58:24 +0530

    implement full image view
    when we click on any image it will be displayed in in full view

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const key = 'jhbj';
+const key = 'jjhvjhi';
 const input = document.getElementById('search-input')
 const imagesContainer = document.querySelector('.search-results');
 const saerchButton = document.getElementById('search-button');
 const showMore = document.getElementById('show-more-button');
+
+
 let query;
 let pageNumber = 1;
 
@@ -11,22 +13,34 @@ function images(pageNumber) {
     .then((value) => {
         return value.json();
     }).then((data) => {
+        if(data.total>0){
         data.results.forEach((element) => {
             let  html = `<div class="search-result">
-                            <img src="${element.urls.regular}" alt="${element.alt_description}">
+                            <img src="${element.urls.regular}" alt="${element.alt_description}" data-image-id="${element.urls.regular}" data-image-description="${element.alt_description}" >
                             <a href="${element.links.html}" target="_blank">${element.alt_description}</a>
-                        </div>`;
-                    imagesContainer.innerHTML += html;
+                        </div>`;    
+            imagesContainer.innerHTML += html;
         });
+        document.querySelectorAll('.search-result')
+            .forEach((images) => {
+                images.children[0].addEventListener('click', () => {
+                    let imageId = images.children[0].dataset.imageId;
+                    const description = images.children[1].textContent;
+                    fullimage(imageId, description)
+                });
+            });
+        }
+        return 0
     });
 }
 
-
-
 saerchButton.addEventListener('click', () => {
-    imagesContainer.innerHTML = '';
     query = input.value;
-    images(pageNumber) 
+    if (!(query === '')) {
+        imagesContainer.innerHTML = '';
+        images(pageNumber)
+        showMore.style.display = 'block'
+    }
 });
 
 document.body.addEventListener('keydown', (event) => {
@@ -35,6 +49,7 @@ document.body.addEventListener('keydown', (event) => {
         if (!(query === '')) {
             imagesContainer.innerHTML = '';
             images(pageNumber)
+            showMore.style.display = 'block'
         }
     }
 });
@@ -43,5 +58,22 @@ showMore.addEventListener('click', () => {
     pageNumber += 1;
     images(pageNumber)
 })
+
+
     
 
+function fullimage(imageId,description){
+        
+        const frame = document.getElementById("frame");
+        frame.style.display = 'block'
+        const closeButton = document.getElementById("close-button");
+    const image = document.getElementById("image");
+    const heading = document.querySelector('.head')
+        closeButton.addEventListener("click", () => {
+            frame.style.display = "none";
+        });
+    image.src = `${imageId}`;
+    image.alt = `${description}`
+    heading.textContent = `${description}`
+    
+}
